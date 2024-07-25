bd689cac4344e40d47f848bb37f35c2e467c807e
Ariko <philemonarik@gmail.com>
2024-07-25 15:46:09 +0300

    ss

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ import {fileURLToPath} from 'url'
 import {dirname} from "path"
 
 //ENVIRONMENT
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 7000
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
 //Middleware
-app.use(express.static(__dirname + '/react-app/build'))
+app.use(express.static(__dirname + '/react-app/build/'))
 
 //Serve index.html for all other get requests
 app.get('*', (req, res) => {
